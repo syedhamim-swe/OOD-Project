067c9ae5af913195ee52b5173be817257bfbc3ba
Syed Hamim <145212620+syedhamim-swe@users.noreply.github.com>
2024-06-12 01:50:49 +0600

    Project

---
+++ test/globaldoctor/InsertUpdateDelete.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package globaldoctor;
+
+/**
+ *
+ * @author Ratul
+ */
+class InsertUpdateDelete {
+
+    static void setData(String query, String register_Succesfully) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
